71fb34eafe0f592ea893293b21737fbd5eda2fcf
Berina Muhović <bmuhovic1@etf.unsa.ba>
2018-04-01 21:29:24 +0200

    added role controller and service

--- microservices/user_management_ms/src/main/java/com/user_managment/ms/Controllers/RoleController.java
+++ microservices/user_management_ms/src/main/java/com/user_managment/ms/Controllers/RoleController.java
@@ -1,4 +1,39 @@
 package com.user_managment.ms.Controllers;
 
+import com.user_managment.ms.Models.Role;
+import com.user_managment.ms.Services.RoleService;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+
+    private RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @RequestMapping("/all")
+    public ResponseEntity all() throws ServiceException {
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity getUser(@RequestParam(value = "id") String id) {
+        return ResponseEntity.ok(roleService.getRole(id));
+    }
+
+    @RequestMapping(value="/create", method = RequestMethod.POST)
+    public ResponseEntity createRole(@RequestBody Role role) {
+        return ResponseEntity.ok(roleService.createRole(role));
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteRole(@PathVariable("id") String id) {
+        return ResponseEntity.ok(roleService.deleteRole(id));
+    }
+
 }
--- microservices/user_management_ms/src/main/java/com/user_managment/ms/Services/RoleService.java
+++ microservices/user_management_ms/src/main/java/com/user_managment/ms/Services/RoleService.java
@@ -1,4 +1,58 @@
 package com.user_managment.ms.Services;
 
+import com.user_managment.ms.Models.Role;
+import com.user_managment.ms.Repository.UserRoleRepository;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class RoleService {
+
+    private final UserRoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(UserRoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public List<Role> getAllRoles() throws ServiceException {
+        try {
+            List<Role> roles = roleRepository.findAll();
+            return roles;
+        } catch (Exception e) {
+            throw new ServiceException("Cannot fetch all roles.");
+        }
+    }
+
+    public Role getRole(String id) throws ServiceException {
+        try {
+            Optional role = roleRepository.findById(Integer.parseInt(id));
+            Role r = (Role) role.get();
+            return r;
+        } catch (Exception e) {
+            throw new ServiceException("Cannot find role with id={" + id + "}");
+        }
+    }
+
+    public Role createRole(Role role) throws ServiceException {
+        try {
+            Role r = roleRepository.save(role);
+            return r;
+        } catch (Exception e) {
+            throw new ServiceException("Cannot create role");
+        }
+    }
+
+    public String deleteRole(String id) throws ServiceException {
+        try {
+            roleRepository.deleteById(Integer.parseInt(id));
+            return "Role deleted";
+        } catch (Exception e) {
+            throw new ServiceException("Cannot delete role with id={" + id + "}");
+        }
+    }
 }
