f11003f753634e7acffcde1d4c9a7664af19e574
emir16949 <ebarucija1@etf.unsa.ba>
2018-05-08 22:13:31 +0200

    docker

--- microservices/Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ microservices/Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
