6fae775606e16d38273e26a1752c57dd64727e4c
Berina Muhović <bmuhovic1@etf.unsa.ba>
2018-05-17 22:55:56 +0200

    added routing

--- frontend/webapp/src/app/app.module.ts
+++ frontend/webapp/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MuzikaComponent } from './muzika/muzika.component';
+import {RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'muzika', component: MuzikaComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuzikaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/webapp/src/app/muzika/muzika.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-muzika',
+  templateUrl: './muzika.component.html',
+  styleUrls: ['./muzika.component.css']
+})
+export class MuzikaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
