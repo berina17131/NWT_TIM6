afb188afb8df330f4201cdfd15c0a5b74d0fb896
emir16949 <ebarucija1@etf.unsa.ba>
2018-05-28 17:41:40 +0200

    Fixed sending CRUD requests for User microservice

--- microservices/user_management_ms/src/main/java/com/user_managment/ms/Models/User.java
+++ microservices/user_management_ms/src/main/java/com/user_managment/ms/Models/User.java
@@ -3,7 +3,6 @@ package com.user_managment.ms.Models;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Entity
