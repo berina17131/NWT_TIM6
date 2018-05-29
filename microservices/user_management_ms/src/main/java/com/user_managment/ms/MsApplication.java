666a62b75db1426de5fab1c3c633dc54d36d4902
emir16949 <ebarucija1@etf.unsa.ba>
2018-05-29 15:53:55 +0200

    Added securities in pom

--- microservices/user_management_ms/src/main/java/com/user_managment/ms/MsApplication.java
+++ microservices/user_management_ms/src/main/java/com/user_managment/ms/MsApplication.java
@@ -3,7 +3,6 @@ package com.user_managment.ms;
 import com.user_managment.ms.Models.Role;
 import com.user_managment.ms.Models.User;
 import com.user_managment.ms.Repository.UserRoleRepository;
-import com.user_managment.ms.Security.UpdatableBCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.transaction.Transactional;
 import java.util.HashSet;
@@ -23,7 +23,7 @@ import java.util.Set;
 public class MsApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(MsApplication.class);
-    private static final UpdatableBCrypt bcrypt = new UpdatableBCrypt(11);
+    private final BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
 
     @Autowired
     private UserRoleRepository roleRepository;
@@ -36,14 +36,14 @@ public class MsApplication implements CommandLineRunner {
     @Transactional
     public void run(String... strings) throws Exception {
         Role user = new Role("ROLE_USER");
-        User user1 = new User("Emir", "Barucija", "emir", bcrypt.hash("password"), user);
+        User user1 = new User("Emir", "Barucija", "emir", bcrypt.encode("password"), user);
         Set ordinary_users = new HashSet<User>() {{
             add(user1);
         }};
         user.setUsers(ordinary_users);
 
         Role admin = new Role("ROLE_ADMIN");
-        User user2 = new User("Amra", "Mujcinovic", "amra", bcrypt.hash("password"), admin);
+        User user2 = new User("Amra", "Mujcinovic", "amra", bcrypt.encode("password"), admin);
         Set admin_users = new HashSet<User>() {{
             add(user2);
         }};
